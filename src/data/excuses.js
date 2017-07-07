32e86f751e4c3c5c626ea20ff14466ee7c0a7407
Boralte <legolas0898@gmail.com>
2017-07-07 16:05:57 +0100

    Ping example

---
+++ src/data/excuses.js
@@ -0,0 +1,15 @@
+const excuses = [{
+  id: 1,
+  text: 'Sorry I can\'t come into work today, my sister\'s friend\'s grandfather\'s goldfish died and it was very tragic',
+  categories: [],
+}, {
+  id: 2,
+  text: 'Sorry, I won\'t make it in today, webMD says I\'m dying',
+  categories: [],
+}, {
+  id: 3,
+  text: 'Sorry, a meteor crashed my car, I don\'t think I will make it today',
+  categories: [],
+}];
+
+export default excuses;
--- src/norrisbot.js
+++ src/norrisbot.js
@@ -4,17 +4,18 @@ import {
   isMessageToChannel,
   isFromUser,
   messageContainsText,
-  filterJokesByCategories,
-  pickRandom,
 } from './utils';
-import jokes from './data/jokes';
-import pictures from './data/pictures';
+
+const exec = require('child_process').exec;
 
 const defaultOptions = {
-  triggerOnWords: ['Chuck Norris', 'norrisbot'],
-  specialCategories: ['nerdy'],
-  messageColor: '#590088',
-  usePictures: true,
+  triggerOnWords: ['ping '],
+  includesWords: ['.com', '.it', '.co.uk', '.gov', 'www.'],
+  installTriggerWords: ['install'],
+  installAndRunTrigger: ['install & run'],
+  installIncludesWords: ['/'],
+  excuseTriggerWords: ['/sick'],
+  messageColor: '#ffb6c1',
   logger: console,
   rtmOptions: {},
 };
@@ -26,37 +27,93 @@ const norrisbot = (botToken, options = {}) => {
   const rtm = new RtmClient(botToken, opt.rtmOptions);
   const web = new WebClient(botToken);
 
-  const allowedJokes = filterJokesByCategories(jokes, opt.specialCategories);
+  const postMessage = (channel, text) => {
+    const msgOptions = {
+      as_user: true,
+      attachments: [
+        {
+          color: opt.messageColor,
+          title: text,
+        },
+      ],
+    };
+
+    web.chat.postMessage(channel, '', msgOptions);
+    opt.logger.info(`Posting message to ${channel}`, msgOptions);
+  };
+
   rtm.on(RTM_EVENTS.MESSAGE, (event) => {
+    console.log(event);
     if (
       isMessage(event) &&
       isMessageToChannel(event) &&
       !isFromUser(event, botId) &&
-      messageContainsText(event, opt.triggerOnWords)
+      messageContainsText(event, opt.triggerOnWords) &&
+      messageContainsText(event, opt.includesWords)
     ) {
-      const joke = pickRandom(allowedJokes);
-      const msgOptions = {
-        as_user: true,
-        attachments: [
-          {
-            color: opt.messageColor,
-            title: joke.text,
-          },
-        ],
-      };
-
-      if (opt.usePictures) {
-        msgOptions.attachments[0].image_url = pickRandom(pictures);
-      }
-
-      web.chat.postMessage(event.channel, '', msgOptions);
-      opt.logger.info(`Posting message to ${event.channel}`, msgOptions);
+      let message = event.text.match(/\|(.*)>/);
+      message = message ? message.pop() : null;
+      if (!message) return postMessage(event.channel, 'No url found');
+      console.log(message);
+      exec(`ping -c 1 ${message}`, (error, stdout, stderr) => {
+        console.log(error, stdout, stderr);
+        const status = error ? error.message : stdout;
+        postMessage(event.channel, status);
+      });
+    }
+    if (
+      isMessage(event) &&
+      isMessageToChannel(event) &&
+      !isFromUser(event, botId) &&
+      messageContainsText(event, opt.installTriggerWords)
+    ) {
+      // let message = event.text.match(/\|(.*)>/);
+      // message = message ? message.pop() : null;
+      const message = event.text.substring(7);
+      if (!message) return postMessage(event.channel, 'No dir found, enclose <dir>');
+      console.log(message);
+      exec(`cd ${message} && npm i`, (error, stdout, stderr) => {
+        console.log(error, stdout, stderr);
+        if (error) {
+          postMessage(event.channel, error.message);
+        } else {
+          postMessage(event.channel, `installed ${stdout}`);
+        }
+      });
+    }
+    if (
+      isMessage(event) &&
+      isMessageToChannel(event) &&
+      !isFromUser(event, botId) &&
+      messageContainsText(event, opt.installAndRunTrigger)
+    ) {
+      // let message = event.text.match(/\|(.*)>/);
+      // message = message ? message.pop() : null;
+      const message = event.text.substring(13);
+      if (!message) return postMessage(event.channel, 'No dir found, enclose <dir>');
+      console.log(message);
+      exec(`cd ~/${message} && npm i && npm start`, (error, stdout, stderr) => {
+        console.log(error, stdout, stderr);
+        if (error) {
+          postMessage(event.channel, error.message);
+        } else {
+          postMessage(event.channel, `installed ${stdout}`);
+          exec(`cd ~/${message} && npm start`, (err, stdo, stde) => {
+            console.log(err, stdo, stde);
+            if (err) {
+              postMessage(event.channel, err.message);
+            } else {
+              postMessage(event.channel, `installed ${stdo}`);
+            }
+          });
+        }
+      });
     }
   });
 
   rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
     botId = rtmStartData.self.id;
-    opt.logger.info(`Logged in as ${rtmStartData.self.name} (id: ${botId}) of team ${rtmStartData.team.name}`);
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
   });
 
   return {
